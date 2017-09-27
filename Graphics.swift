e02f6a5b6e2b65b76e8028b5a4de593ab449e299
Juan Carlos Díaz Torres <juancarlos@Juans-Mac.local>
2017-09-27 23:59:02 +0200

    Riego automático v2.0

--- Graphics.swift
+++ Graphics.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 protocol FuncProtocol {
-    func start() -> Double //Inicio
-    func end() -> Double //Final
-    func pointAt(index: Double) -> FunctionPoint
+    func startFor(_ graphic : Graphics) -> Double //Inicio
+    func endFor(_ graphic : Graphics) -> Double //Final
+    func nextPoint(_ graphic : Graphics, pointAt index: Double) -> FunctionPoint
 }
 
 struct FunctionPoint {
@@ -21,32 +21,54 @@ struct FunctionPoint {
 
 
 class Graphics: UIView {
-    
-    //Objeto para start y end. Comunico Graphics con ViewController
     var dataSource : FuncProtocol!
+    var scaleX = 1.0
+    var scaleY = 1.0
+    
     
     override func draw(_ rect: CGRect) {
+        axis()
+        trajectory()
+    }
+    
+    private func axis() {
+        let w = bounds.width
+        let h = bounds.height
+        let pathy = UIBezierPath()
+        let pathx = UIBezierPath()
         
+        pathy.move(to: CGPoint(x : w/2, y : 0.0))
+        pathy.addLine(to: CGPoint(x : w/2, y : h))
+        pathy.lineWidth = 2
+        UIColor.black.setStroke()
+        pathy.stroke()
+        
+        pathx.move(to: CGPoint(x : 0, y : h/2))
+        pathx.addLine(to: CGPoint(x : w, y : h/2))
+        pathx.lineWidth = 2
+        UIColor.black.setStroke()
+        pathx.stroke()
+        
+    }
+    
+    private func trajectory() {
         //Path vacío
         let path = UIBezierPath()
         
-        //Posicionamiento NPI de como colocarlos
+        //Posicionamiento
         let h = bounds.height
         let w = bounds.width
+        path.move(to: CGPoint(x: w/2, y: h/2))
         
         //Variables path (0, 200 y rango)
-        let P0 = dataSource.start()
-        let PF = dataSource.end()
-        let deltaP = (PF - P0) / 100
-        
-        //Continuo el path
-        path.move(to: CGPoint(x: 0.0, y: 0.0))
+        let P0 = dataSource.startFor(self)
+        let PF = dataSource.endFor(self)
 
-        for p in stride(from: P0, to: PF, by: deltaP) {
+        for p in stride(from: P0, to: PF, by: 0.1) {
         
-            let v = dataSource.pointAt(index: p)
-            let x = v.x
-            let y = v.y
+            let v = dataSource.nextPoint(self, pointAt: p)
+            let x = scalingX(v.x)
+            let y = scalingY(v.y)
             
             let punto = CGPoint(x: x, y: y)
             
@@ -60,6 +82,18 @@ class Graphics: UIView {
         
         //pinta
         path.stroke()
-
 }
+    
+    private func scalingX(_ scaleInX : Double) ->Double {
+        let weight = Double(bounds.width)
+        return ((weight/2) + (scaleInX*scaleX))
+        
+    }
+    
+    private func scalingY(_ scaleInY : Double) ->Double {
+        let height = Double(bounds.height)
+        return ((height/2) - (scaleInY*scaleY))
+        
+    }
+    
 }
--- ViewController.swift
+++ ViewController.swift
@@ -11,34 +11,66 @@ import UIKit
 class ViewController: UIViewController, FuncProtocol {
     
     @IBOutlet weak var graphic1: Graphics!
-    
-
-    func start() -> Double {
-        return 0.0
-    }
-    
-    func end() -> Double {
-        return 200.0
-    }
-    
-    func pointAt(index: Double) -> FunctionPoint {
-        return FunctionPoint(x: index, y: 150*cos(index))
-    }
-    
-
+    @IBOutlet weak var graphic2: Graphics!
+    @IBOutlet weak var graphic3: Graphics!
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Conexion entre clases
         graphic1.dataSource = self
+        graphic2.dataSource = self
+        graphic3.dataSource = self
+        
+        //Podria acceder aqui a las escalas
+        graphic1.scaleX = 1.0
+        graphic1.scaleY = Double(slider.value)
+        graphic2.scaleX = 1.0
+        graphic1.scaleY = Double(slider.value)
+        graphic3.scaleX = 1.0
+        graphic1.scaleY = Double(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Control del slider
+    
+    @IBAction func sliderScale(_ sender: UISlider) {
+        graphic1.scaleY = Double(slider.value)
+        graphic1.setNeedsDisplay()
 
-
+        graphic2.scaleY = Double(slider.value)
+        graphic2.setNeedsDisplay()
+        
+        graphic3.scaleY = Double(slider.value)
+        graphic3.setNeedsDisplay()
+    }
+    
+    
+    //Protocolo
+    func startFor(_ graphic : Graphics) -> Double {
+        return 0
+    }
+    
+    func endFor(_ graphic : Graphics) -> Double {
+        return 300.0
+    }
+    
+    func nextPoint(_ graphic: Graphics, pointAt index: Double) -> FunctionPoint {
+        switch graphic {
+        case graphic1:
+            return FunctionPoint(x: index, y: sin(index))
+        case graphic2:
+            return FunctionPoint(x: index, y: tan(index))
+        case graphic3:
+            return FunctionPoint(x: index, y: exp(-1000*index))
+        default:
+            return FunctionPoint(x: 0.0, y: 0.0)
+        }
+    }
 }
 
